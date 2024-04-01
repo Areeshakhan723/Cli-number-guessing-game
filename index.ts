2f3457eed330b85b46ab481e8aa14e02ad427182
Areeshakhan723 <areshakhan6400@gmail.com>
2024-04-02 02:29:16 +0500

    created CLI_Based Number Guessing Game

---
+++ index.ts
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const gameNumber = Math.floor(Math.random() * 5 + 1); 
+
+const answers = await inquirer.prompt([{
+    name : "userGuessNumber",
+    type : "number",
+    message : "please guess a number between 1-5:",
+    },
+]);
+
+console.log(answers);
+
+if(answers.userGuessNumber === gameNumber){
+    console.log("Congulations! you guess a write number")
+}else{
+    console.log("try again! you guess a wrong number")
+};
